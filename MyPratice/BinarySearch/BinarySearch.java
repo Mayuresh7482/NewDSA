601647878b54cb4cdd972878088055893e57858e
benStokes018 <sohan.cdac@gmail.com>
2024-06-29 02:33:20 +0530

    29

---
+++ MyPratice/BinarySearch/BinarySearch.java
@@ -0,0 +1,26 @@
+public class BinarySearch {
+
+    static boolean BinarySearch(int a[], int target) {
+        int n = a.length;
+        int start = 0, end = n - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (a[mid] == target)
+                return true;
+            if (a[mid] < target)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+        int target = 0;
+        while(target!=10){
+            System.out.printf(" %d exists in array %b \n",target,BinarySearch(arr, target));
+            target++;
+        }
+    }
+}
